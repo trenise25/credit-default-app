e8a76077b00eef3c438816a40cfe3c32ece9753a
trenise25 <trenisedsouza255@gmail.com>
2025-07-20 17:10:10 +0530

    Fix: matched Streamlit inputs to trained model features

--- app.py
+++ app.py
@@ -15,18 +15,37 @@ scaler = joblib.load("scaler.pkl")
 with st.form("credit_form"):
     LIMIT_BAL = st.number_input("Credit Limit (LIMIT_BAL)", value=20000)
     AGE = st.number_input("Age", min_value=18, max_value=100, value=30)
-    SEX = st.selectbox("Gender (SEX)", [1, 2])
+    SEX = st.selectbox("Gender (1=Male, 2=Female)", [1, 2])
     EDUCATION = st.selectbox("Education (1=Grad, 2=Univ, 3=HighSchool, 4=Other)", [1, 2, 3, 4])
     MARRIAGE = st.selectbox("Marriage (1=Married, 2=Single, 3=Other)", [1, 2, 3])
-    PAY_0 = st.slider("Last Repayment Status (PAY_0)", -2, 8, value=0)
-    BILL_AMT1 = st.number_input("Last Month Bill Amount (BILL_AMT1)", value=5000)
+    PAY_0 = st.slider("Repayment Status in September (PAY_0)", -2, 8, value=0)
+    PAY_2 = st.slider("Repayment Status in August (PAY_2)", -2, 8, value=0)
+    PAY_3 = st.slider("Repayment Status in July (PAY_3)", -2, 8, value=0)
+    PAY_4 = st.slider("Repayment Status in June (PAY_4)", -2, 8, value=0)
+    PAY_5 = st.slider("Repayment Status in May (PAY_5)", -2, 8, value=0)
+    PAY_6 = st.slider("Repayment Status in April (PAY_6)", -2, 8, value=0)
+    
+    BILL_AMT1 = st.number_input("Bill Amount for September", value=5000)
+    BILL_AMT2 = st.number_input("Bill Amount for August", value=4000)
+    BILL_AMT3 = st.number_input("Bill Amount for July", value=3000)
+    BILL_AMT4 = st.number_input("Bill Amount for June", value=2000)
+    BILL_AMT5 = st.number_input("Bill Amount for May", value=1000)
+    BILL_AMT6 = st.number_input("Bill Amount for April", value=500)
+
+    PAY_AMT1 = st.number_input("Payment Amount in September", value=1000)
+    PAY_AMT2 = st.number_input("Payment Amount in August", value=800)
+    PAY_AMT3 = st.number_input("Payment Amount in July", value=600)
+    PAY_AMT4 = st.number_input("Payment Amount in June", value=400)
+    PAY_AMT5 = st.number_input("Payment Amount in May", value=200)
+    PAY_AMT6 = st.number_input("Payment Amount in April", value=100)
 
     submitted = st.form_submit_button("Predict")
 
 if submitted:
     input_data = np.array([[LIMIT_BAL, SEX, EDUCATION, MARRIAGE, AGE,
-                            PAY_0, 0, 0, 0, 0, 0, 0, BILL_AMT1, 0, 0, 0, 0, 0, 0,
-                            0, 0, 0, 0, 0]])
+                            PAY_0, PAY_2, PAY_3, PAY_4, PAY_5, PAY_6,
+                            BILL_AMT1, BILL_AMT2, BILL_AMT3, BILL_AMT4, BILL_AMT5, BILL_AMT6,
+                            PAY_AMT1, PAY_AMT2, PAY_AMT3, PAY_AMT4, PAY_AMT5, PAY_AMT6]])
 
     input_scaled = scaler.transform(input_data)
     prob = model.predict_proba(input_scaled)[0][1]
